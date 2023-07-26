a1ee87ffe2ed7b3ee185092f658bc1b34e71c86d
indi-stack <indi.fraga08@gmail.com>
2023-07-25 18:48:19 -0700

    Updated the label effect when active

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const quoteEl = document.getElementById("quote");
 const authorEl = document.getElementById("author");
 const btnEl = document.getElementById("btn");
-const catEl = document.querySelectorAll(".category");
+const catEls = document.querySelectorAll("label");
 let radioVal;
 const apiKey = "qIMeav8yHdFdhBrm6jPpmQ==Z3egrkOuajdNkHtS";
 const options = {
@@ -12,9 +12,10 @@ const options = {
 
 async function getQuote() {
     try {
-        btnEl.innerText = "...loading";
-        btnEl.disabled = true;
+
         if (radioVal) {
+            btnEl.innerText = "...loading";
+            btnEl.disabled = true;
             const url = `https://api.api-ninjas.com/v1/quotes?category=${radioVal}`;
             const response = await fetch(url, options);
             const result = await response.json();
@@ -25,32 +26,32 @@ async function getQuote() {
             btnEl.innerText = "Get Quote";
             btnEl.disabled = false;
         }
-        else {
-            quoteEl.innerHTML = `please select a category`;
-            authorEl.innerText = ``;
-            btnEl.innerText = "Get Quote";
-            btnEl.disabled = false;
-        }
+        
     } catch (error) {
         console.error(error);
         quoteEl.innerHTML = `There was an error, please try again later`;
         authorEl.innerText = `try again`;
         btnEl.innerText = "Get Quote";
-            btnEl.disabled = false;
+        btnEl.disabled = false;
     }
 }
 
 //getting which radio button is checked and storing its value
 function handleRadioButtonClick(event) {
     radioVal = event.target.value;
+
+    catEls.forEach((catEl) => {
+        catEl.classList.remove("active");
+    });
+
+    event.target.classList.add("active");
+    event.target.closest('label').classList.add("active");
+
 }
 
-
-catEl.forEach(el => {
+catEls.forEach(el => {
     el.addEventListener('click', handleRadioButtonClick);
 });
-
-
 
 btnEl.addEventListener("click", getQuote);
 
